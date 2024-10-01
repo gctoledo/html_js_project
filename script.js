af339ac29151215c91118f05d76e754940fb9e8e
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-01 19:37:42 -0300

    feat: add pagination feature

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const planetsDiv = document.getElementById('planets')
+const nextBtn = document.getElementById('next')
+const prevBtn = document.getElementById('previous')
 
 const apiUrl = 'https://swapi.dev/api/planets/'
 
 let currentPageUrl = 'https://swapi.dev/api/planets/'
+let nextPageUrl = null
+let prevPageUrl = null
+let currentPage = 1
 
 const showPlanets = async (url) => {
 
   try {
     const response = await fetch(url)
     const data = await response.json()
+
+    nextPageUrl = data.next
+    prevPageUrl = data.previous
+    
+    nextBtn.disabled = !nextPageUrl
+    prevBtn.disabled = !prevPageUrl
 
     planetsDiv.innerHTML = ''
 
@@ -24,5 +35,19 @@ const showPlanets = async (url) => {
     console.error(err)
   }
 }
+
+nextBtn.addEventListener('click', () => {
+  if (nextPageUrl) {
+    currentPage++
+    showPlanets(nextPageUrl)
+  }
+})
+
+prevBtn.addEventListener('click', () => {
+  if (prevPageUrl) {
+    currentPage--
+    showPlanets(prevPageUrl)
+  }
+})
 
 showPlanets(currentPageUrl)
